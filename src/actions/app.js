df1873ba3b004e18b97404235278fe077e6c329c
Luke Simonetti <lsimonetti@startchurch.com>
2018-10-22 22:21:55 -0400

    Get time slot action creator

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,1 +1,13 @@
-//app.js action code here
+import axios from 'axios'
+
+export const getAvailableTimeSlots = () => (dispatch) => {
+
+    axios.get('/api/available-time-slots')
+    .then(res => {
+        const data = res.data
+        dispatch({type: 'APP:SET_DATA', key: 'availableTimeSlots', val: data})
+    })
+    .catch(err => {
+
+    })
+}
