92f8218f68c1e0718f50acf8296201434b05c750
Luke Simonetti <lsimonetti@startchurch.com>
2018-10-24 18:56:00 -0400

    Renaming route

--- src/actions/app.js
+++ src/actions/app.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const getAvailableTimeSlots = () => (dispatch) => {
 
-    axios.get('/api/available-time-slots')
+    axios.get('/api/time-slots')
     .then(res => {
         const data = res.data
         dispatch({type: 'APP:SET_DATA', key: 'availableTimeSlots', val: data})
