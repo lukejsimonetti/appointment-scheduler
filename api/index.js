b4cc08b93a24b8aa782f3a867175f26021ebb351
Luke Simonetti <lsimonetti@startchurch.com>
2018-10-24 18:55:15 -0400

    Fixing routes

--- api/index.js
+++ api/index.js
@@ -3,16 +3,31 @@ const app = express()
 const port = 3001
 const timeSlots = require('./timeSlots.json')
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+let timeSlotsAvailable = timeSlots
+
+const mutateTimeSlotData = (timeSlotId, data) => {
+    const targetSlot = getTimeSlotById(timeSlotId)
+    targetSlot.data = {...data}
+
+    return timeSlotsAvailable
+}
+
+const getTimeSlotById = (timeSlotId) => {
+    return timeSlotsAvailable.filter(x => x.time_slot_id == timeSlotId)
+}
     
-app.get('/api/available-time-slots', (req, res) => {
-    res.send(timeSlots)
+app.get('/api/time-slots', (req, res) => {
+    // data retrieval from database
+    res.send(timeSlotsAvailable)
+})
+ 
+app.get('/api/time-slot/:id', (req, res) => {
+    const timeSlotId = req.params.id
+    res.send(getTimeSlotById(timeSlotId))
 })
 
-// app.get('/', (req, res) => {
-//     res.send(timeSlots)
-// })
+app.put('/api/time-slot', (req, res) => {
+    res.send(mutateTimeSlotData(req.body.id, req.body.time_slot_data))
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port)
