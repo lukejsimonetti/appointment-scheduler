669a22ff1f4d25f821d6f88df2596657b6fd88b7
Luke Simonetti <lsimonetti@startchurch.com>
2018-10-25 07:53:14 -0400

    Is time slot available function

--- src/views/TimeSlotRow.js
+++ src/views/TimeSlotRow.js
@@ -3,12 +3,16 @@ import PropTypes from 'prop-types';
 import moment from 'moment'
 import {Button} from 'reactstrap'
 
+const isAvailable = (data) => {
+    return Boolean(Object.keys(data).length === 0)
+}
+
 const TimeSlotRow = (props) => {
     return (
         <tr>
-            <td className="text-center">{moment(props.value, "hh:mm").format("hh:mm").replace(/^0(?:0:0?)?/, '')}</td>
+            <td className="text-center">{moment(props.value, "hh:mm").format("hh:mm A").replace(/^0(?:0:0?)?/, '')}</td>
             <td>
-                <Button color="success"
+                <Button color={isAvailable(props.data) ? "success" : "danger"}
                     onClick={() => props.handleModal('TIME_SLOT_MODAL', props.data, props.time_slot_id)}> Schedule Appointment 
                 </Button>
                 </td>
